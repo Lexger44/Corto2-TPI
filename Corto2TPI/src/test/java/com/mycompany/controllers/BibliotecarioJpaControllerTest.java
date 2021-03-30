07ef4ca71f8c12ef0b8b901db8c96c159660e1fc
mm18018 <mm18018@ues.edu.sv>
2021-03-30 02:08:42 -0600

    TestEdit BibliotecarioController

--- Corto2TPI/src/test/java/com/mycompany/controllers/BibliotecarioJpaControllerTest.java
+++ Corto2TPI/src/test/java/com/mycompany/controllers/BibliotecarioJpaControllerTest.java
@@ -5,8 +5,10 @@
  */
 package com.mycompany.controllers;
 
+import com.mycompany.controllers.exceptions.NonexistentEntityException;
 import com.mycompany.entity.Bibliotecario;
 import java.util.List;
+import javax.management.RuntimeErrorException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -24,31 +26,31 @@ import org.mockito.junit.jupiter.MockitoExtension;
  */
 @ExtendWith(MockitoExtension.class)
 public class BibliotecarioJpaControllerTest {
-    
+
     static EntityManagerFactory mockEmf;
     static BibliotecarioJpaController registro;
     static Bibliotecario bibliotecarioObject;
-    
+
     public BibliotecarioJpaControllerTest() {
     }
-    
+
     @BeforeAll
-    public static void setUpClass() {        
-        mockEmf = Mockito.mock(EntityManagerFactory.class);        
+    public static void setUpClass() {
+        mockEmf = Mockito.mock(EntityManagerFactory.class);
         registro = new BibliotecarioJpaController(mockEmf);
         bibliotecarioObject = new Bibliotecario(1L);
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-        
+
     /**
      * Test of getEntityManager method, of class BibliotecarioJpaController.
      */
     @Test
     public void testGetEntityManager() {
-        System.out.println("getEntityManager");     
+        System.out.println("getEntityManager");
         EntityManager mockEm = Mockito.mock(EntityManager.class);
         Mockito.when(mockEmf.createEntityManager()).thenReturn(mockEm);
         EntityManager expResult = mockEm;
@@ -61,37 +63,91 @@ public class BibliotecarioJpaControllerTest {
      */
     @Test
     public void testCreate() {
-        System.out.println("create");    
+        System.out.println("create");
         EntityManager mockEm = Mockito.mock(EntityManager.class);
         EntityTransaction mockTx = Mockito.mock(EntityTransaction.class);
         Mockito.when(mockEmf.createEntityManager()).thenReturn(mockEm);
-        Mockito.when(mockEm.getTransaction()).thenReturn(mockTx);        
+        Mockito.when(mockEm.getTransaction()).thenReturn(mockTx);
         this.registro.create(bibliotecarioObject);
-        Mockito.verify(mockEm).persist(this.bibliotecarioObject);        
-        Mockito.verify(mockEm).close();        
+        Mockito.verify(mockEm).persist(this.bibliotecarioObject);
+        Mockito.verify(mockEm).close();
     }
 
     /**
      * Test of edit method, of class BibliotecarioJpaController.
      */
     @Test
-    public void testEdit() throws Exception {
+    public void testEdit() throws Exception, NonexistentEntityException {
         System.out.println("edit");
         EntityManager mockEm = Mockito.mock(EntityManager.class);
         EntityTransaction mockTx = Mockito.mock(EntityTransaction.class);
         Mockito.when(mockEmf.createEntityManager()).thenReturn(mockEm);
-        Mockito.when(mockEm.getTransaction()).thenReturn(mockTx); 
-                
-        Mockito.when(mockEm.merge(bibliotecarioObject)).thenReturn(bibliotecarioObject);        
+        Mockito.when(mockEm.getTransaction()).thenReturn(mockTx);
+
+        Mockito.when(mockEm.merge(bibliotecarioObject)).thenReturn(bibliotecarioObject);
         registro.edit(bibliotecarioObject);
         Mockito.verify(mockEm).merge(bibliotecarioObject);
+        Mockito.verify(mockEm).close();
+        RuntimeErrorException mockRx = Mockito.mock(RuntimeErrorException.class);
         
-        
+        Mockito.when(mockEm.merge(bibliotecarioObject)).thenThrow(mockRx);
+        Bibliotecario mockB = Mockito.mock(Bibliotecario.class);        
+        try {
+            registro.edit(bibliotecarioObject);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+               
+        Mockito.when(mockRx.getLocalizedMessage()).thenReturn("Excepcion esperada");
+
+        try {
+            registro.edit(bibliotecarioObject);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     /**
      * Test of destroy method, of class BibliotecarioJpaController.
      */
-    
-    
+    @Test
+    public void testDestroy() throws Exception {
+        System.out.println("destroy");
+        Long id = null;
+        BibliotecarioJpaController instance = null;
+        instance.destroy(id);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of findBibliotecario method, of class BibliotecarioJpaController.
+     */
+    @Test
+    public void testFindBibliotecario() {
+        System.out.println("findBibliotecario");
+        Long id = null;
+        BibliotecarioJpaController instance = null;
+        Bibliotecario expResult = null;
+        Bibliotecario result = instance.findBibliotecario(id);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getBibliotecarioCount method, of class
+     * BibliotecarioJpaController.
+     */
+    @Test
+    public void testGetBibliotecarioCount() {
+        System.out.println("getBibliotecarioCount");
+        BibliotecarioJpaController instance = null;
+        int expResult = 0;
+        int result = instance.getBibliotecarioCount();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
 }
