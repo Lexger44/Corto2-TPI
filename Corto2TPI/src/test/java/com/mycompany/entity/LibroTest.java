180e29f71d92b5e34c4922404305bd61c5462635
VM18042 <vm18042@ues.edu.sv>
2021-03-31 22:41:56 -0600

    Terminando los test de la entity libro al 100%

--- Corto2TPI/src/test/java/com/mycompany/entity/LibroTest.java
+++ Corto2TPI/src/test/java/com/mycompany/entity/LibroTest.java
@@ -17,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+//import java.util.Set;
 
 /**
  *
@@ -26,17 +27,27 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class LibroTest {
 
     static Libro registro = new Libro();
-    Date fechaprueba = new Date(121, 10, 11,16,30,10);
+    Date fechaprueba;
+
     public LibroTest() {
+        fechaprueba = new Date(121, 10, 11, 16, 30, 10);
     }
 
     @BeforeAll
     public static void setUpClass() throws ParseException {
         registro.setAutor("Paquito");
-        Date fechaprueba = new Date(121, 10, 11,16,30,10);
+        Date fechaprueba = new Date(121, 10, 11, 16, 30, 10);
         System.out.println(DateFormat.getDateInstance().format(fechaprueba));
         registro.setFechaAlta(fechaprueba);
         registro.setIsbn("Isbnprueba");
+        registro.setNumDisponibles(1);
+        registro.setNumPaginas(1);
+        registro.setTitulo("PanchoVilla");
+        Set<Ejemplar> ejemplarprueba = Mockito.mock(Set.class);
+        registro.setEjemplarSet(ejemplarprueba);
+        Set<Reserva> reservaprueba = Mockito.mock(Set.class);
+        registro.setReservaSet(reservaprueba);
+
     }
 
     @AfterAll
@@ -52,7 +63,6 @@ public class LibroTest {
         int expResult = 1;
         int result = instance.getId();
         assertEquals(expResult, result);
-        //fail("The test case is a prototype.");
     }
 
     @Test
@@ -61,7 +71,7 @@ public class LibroTest {
         int id = 1;
         Libro instance = Mockito.mock(Libro.class);
         instance.setId(id);
-        //fail("The test case is a prototype.");
+        Mockito.verify(instance).setId(id);
     }
 
     @Test
@@ -70,8 +80,7 @@ public class LibroTest {
         String expResult = "Paquito";
         String result = this.registro.getAutor();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+
     }
 
     @Test
@@ -81,8 +90,7 @@ public class LibroTest {
         Libro instance = Mockito.mock(Libro.class);
         instance.setAutor(autor);
         Mockito.verify(instance).setAutor(autor);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+
     }
 
     @Test
@@ -91,8 +99,9 @@ public class LibroTest {
         Date expResult = fechaprueba;
         Date result = this.registro.getFechaAlta();
         assertEquals(expResult, result);
-        //fail("The test case is a prototype.");
+
     }
+
     @Test
     public void testSetFechaAlta() {
         //preguntarle a margueiz
@@ -101,7 +110,7 @@ public class LibroTest {
         Libro instance = Mockito.mock(Libro.class);
         instance.setFechaAlta(fechaAlta);
         Mockito.verify(instance).setFechaAlta(fechaAlta);
-        //fail("The test case is a prototype.");
+
     }
 
     @Test
@@ -110,13 +119,9 @@ public class LibroTest {
         String expResult = "Isbnprueba";
         String result = this.registro.getIsbn();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+
     }
 
-    /**
-     * Test of setIsbn method, of class Libro.
-     */
     @Test
     public void testSetIsbn() {
         System.out.println("setIsbn");
@@ -124,154 +129,105 @@ public class LibroTest {
         Libro instance = Mockito.mock(Libro.class);
         instance.setIsbn(isbn);
         Mockito.verify(instance).setIsbn(isbn);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
     }
 
+    @Test
+    public void testGetNumDisponibles() {
+        System.out.println("getNumDisponibles");
+        Integer expResult = 1;
+        Integer result = this.registro.getNumDisponibles();
+        assertEquals(expResult, result);
+    }
 
-//    @Test
-//    public void testGetNumDisponibles() {
-//        System.out.println("getNumDisponibles");
-//        Libro instance = new Libro();
-//        Integer expResult = null;
-//        Integer result = instance.getNumDisponibles();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        //fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setNumDisponibles method, of class Libro.
-//     */
-//    @Test
-//    public void testSetNumDisponibles() {
-//        System.out.println("setNumDisponibles");
-//        Integer numDisponibles = null;
-//        Libro instance = new Libro();
-//        instance.setNumDisponibles(numDisponibles);
-//        // TODO review the generated test code and remove the default call to fail.
-//        //fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of getNumPaginas method, of class Libro.
-//     */
-//    @Test
-//    public void testGetNumPaginas() {
-//        System.out.println("getNumPaginas");
-//        Libro instance = new Libro();
-//        Integer expResult = null;
-//        Integer result = instance.getNumPaginas();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        //fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setNumPaginas method, of class Libro.
-//     */
-//    @Test
-//    public void testSetNumPaginas() {
-//        System.out.println("setNumPaginas");
-//        Integer numPaginas = null;
-//        Libro instance = new Libro();
-//        instance.setNumPaginas(numPaginas);
-//        // TODO review the generated test code and remove the default call to fail.
-//        //fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of getTitulo method, of class Libro.
-//     */
-//    @Test
-//    public void testGetTitulo() {
-//        System.out.println("getTitulo");
-//        Libro instance = new Libro();
-//        String expResult = "";
-//        String result = instance.getTitulo();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        //fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setTitulo method, of class Libro.
-//     */
-//    @Test
-//    public void testSetTitulo() {
-//        System.out.println("setTitulo");
-//        String titulo = "";
-//        Libro instance = new Libro();
-//        instance.setTitulo(titulo);
-//        // TODO review the generated test code and remove the default call to fail.
-//        //fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of getEjemplarSet method, of class Libro.
-//     */
-//    @Test
-//    public void testGetEjemplarSet() {
-//        System.out.println("getEjemplarSet");
-//        Libro instance = new Libro();
-//        Set<Ejemplar> expResult = null;
-//        Set<Ejemplar> result = instance.getEjemplarSet();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        //fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setEjemplarSet method, of class Libro.
-//     */
-//    @Test
-//    public void testSetEjemplarSet() {
-//        System.out.println("setEjemplarSet");
-//        Set<Ejemplar> ejemplarSet = null;
-//        Libro instance = new Libro();
-//        instance.setEjemplarSet(ejemplarSet);
-//        // TODO review the generated test code and remove the default call to fail.
-//        //fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of getReservaSet method, of class Libro.
-//     */
-//    @Test
-//    public void testGetReservaSet() {
-//        System.out.println("getReservaSet");
-//        Libro instance = new Libro();
-//        Set<Reserva> expResult = null;
-//        Set<Reserva> result = instance.getReservaSet();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        //fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setReservaSet method, of class Libro.
-//     */
-//    @Test
-//    public void testSetReservaSet() {
-//        System.out.println("setReservaSet");
-//        Set<Reserva> reservaSet = null;
-//        Libro instance = new Libro();
-//        instance.setReservaSet(reservaSet);
-//        // TODO review the generated test code and remove the default call to fail.
-//        //fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of toString method, of class Libro.
-//     */
-//    @Test
-//    public void testToString() {
-//        System.out.println("toString");
-//        Libro instance = new Libro();
-//        String expResult = "";
-//        String result = instance.toString();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        //fail("The test case is a prototype.");
-//    }
+    @Test
+    public void testSetNumDisponibles() {
+        System.out.println("setNumDisponibles");
+        Integer numDisponibles = 1;
+        Libro instance = Mockito.mock(Libro.class);
+        instance.setNumDisponibles(numDisponibles);
+        Mockito.verify(instance).setNumDisponibles(numDisponibles);
+    }
+
+    @Test
+    public void testGetNumPaginas() {
+        System.out.println("getNumPaginas");
+        Integer expResult = 1;
+        Integer result = this.registro.getNumPaginas();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testSetNumPaginas() {
+        System.out.println("setNumPaginas");
+        Integer numPaginas = 1;
+        Libro instance = Mockito.mock(Libro.class);
+        instance.setNumPaginas(numPaginas);
+        Mockito.verify(instance).setNumPaginas(numPaginas);
+    }
+
+    @Test
+    public void testGetTitulo() {
+        System.out.println("getTitulo");
+        String expResult = "PanchoVilla";
+        String result = this.registro.getTitulo();
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testSetTitulo() {
+        System.out.println("setTitulo");
+        String titulo = "PanchoVilla";
+        Libro instance = Mockito.mock(Libro.class);
+        instance.setTitulo(titulo);
+        Mockito.verify(instance).setTitulo(titulo);
+
+    }
+
+    @Test
+    public void testGetEjemplarSet() {
+        System.out.println("getEjemplarSet");
+        Set<Ejemplar> expResult = this.registro.getEjemplarSet();
+        Set<Ejemplar> result = this.registro.getEjemplarSet();
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testSetEjemplarSet() {
+        System.out.println("setEjemplarSet");
+        Set<Ejemplar> libroSet = Mockito.mock(Set.class);;
+        Libro instance = Mockito.mock(Libro.class);
+        instance.setEjemplarSet(libroSet);
+        Mockito.verify(instance).setEjemplarSet(libroSet);
+
+    }
+
+    @Test
+    public void testGetReservaSet() {
+        System.out.println("getReservaSet");
+        Set<Reserva> expResult = this.registro.getReservaSet();
+        Set<Reserva> result = this.registro.getReservaSet();
+        assertEquals(expResult, result);
+//        Mockito.when(instance.getReservaSet()).thenReturn(expResult);
+    }
+
+    @Test
+    public void testSetReservaSet() {
+        System.out.println("setReservaSet");
+        Set<Reserva> libroSet = Mockito.mock(Set.class);;
+        Libro instance = Mockito.mock(Libro.class);
+        instance.setReservaSet(libroSet);
+        Mockito.verify(instance).setReservaSet(libroSet);
+    }
+
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Libro instance = new Libro(1);
+        String expResult = "com.mycompany.entity.Libro[ id=1 ]";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+
+    }
 }
